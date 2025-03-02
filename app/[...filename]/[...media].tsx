0cf1bf194039948e80ecc0ee6d3deb00c64b7a33
Kostadin Draganov <kostadindraganov@gmail.com>
2025-03-02 04:57:14 +0200

    fix

--- app/[...filename]/[...media].tsx
+++ app/[...filename]/[...media].tsx
@@ -7,16 +7,17 @@ import {
   export const config = mediaHandlerConfig;
   
   export default createMediaHandler({
-    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    authorized: async (req) => {
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || "dknctjjlc",
+    api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY || "27775379465381",
+    api_secret: process.env.CLOUDINARY_API_SECRET || "IiGptlMU6hwBGQrFeDGpw1EMlA0",
+    // eslint-disable-next-line no-unused-vars
+    authorized: async (req, res) => {
       if (process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT === "1") {
         return true;
       }
       try {
         const user = await isAuthorized(req);
-        return user && user.verified;
+        return Boolean(user && user.verified);
       } catch (e) {
         console.error(e);
         return false;
